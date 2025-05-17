f5220efa303dc4a3d265b1ad37830ba942deb51d
Rostislav Lazarev <lazarev.rost.a@gmail.com>
2025-05-17 23:34:27 +0300

    first try

--- main.go
+++ main.go
@@ -2,6 +2,9 @@ package main
 
 import (
 	"fmt"
+	"math/rand"
+	"sync"
+	"time"
 )
 
 const (
@@ -10,31 +13,118 @@ const (
 )
 
 // generateRandomElements generates random elements.
-func generateRandomElements(size int) []int {
-	// ваш код здесь
+func generateRandomElements(size int) ([]int, error) {
+	if size <= 0 {
+		err := fmt.Errorf("slice size must have positive integer value")
+		return nil, err
+	}
+
+	data := make([]int, size)
+	for i := range data {
+		data[i] = rand.Int()
+	}
+	return data, nil
 }
 
 // maximum returns the maximum number of elements.
-func maximum(data []int) int {
-	// ваш код здесь
+func maximum(data []int) (int, error) {
+	if len(data) == 0 {
+		return 0, fmt.Errorf("can't find maximum in empty slice")
+	}
+	max := data[0]
+	for _, v := range data {
+		if v > max {
+			max = v
+		}
+	}
+	return max, nil
 }
 
 // maxChunks returns the maximum number of elements in a chunks.
-func maxChunks(data []int) int {
-	// ваш код здесь
+func maxChunks(data []int) (int, error) {
+	var wg sync.WaitGroup
+	var mu sync.Mutex
+
+	if len(data) == 0 {
+		return 0, fmt.Errorf("can't find maximum in empty slice")
+	}
+
+	if len(data) < CHUNKS {
+		fmt.Println("data slice size is smaller than the number of chunks. It is recommended to use search of maximum conducted in one thread")
+	}
+
+	sizeChunk := len(data) / CHUNKS
+	if sizeChunk == 0 {
+		sizeChunk = 1
+	}
+
+	maxVals := make([]int, 0, CHUNKS)
+
+	for i := 0; i < CHUNKS; i++ {
+		start := i * sizeChunk
+		end := start + sizeChunk
+
+		if i == CHUNKS-1 {
+			end = len(data)
+		}
+
+		if start >= len(data) {
+			break
+		}
+
+		wg.Add(1)
+		go func(chunk []int) {
+			defer wg.Done()
+			locMax, err := maximum(chunk)
+			if err != nil {
+				fmt.Println("error in finding maximum with gorutines", err)
+				return
+			}
+			mu.Lock()
+			maxVals = append(maxVals, locMax)
+			mu.Unlock()
+		}(data[start:end])
+	}
+	wg.Wait()
+
+	absMax, err := maximum(maxVals)
+
+	if err != nil {
+		return 0, fmt.Errorf("error in finding maximum in maxSlice: %w", err)
+	}
+
+	return absMax, nil
 }
 
 func main() {
-	fmt.Printf("Генерируем %d целых чисел", SIZE)
-	// ваш код здесь
+
+	fmt.Printf("Генерируем %d целых чисел\n", SIZE)
+	data, err := generateRandomElements(SIZE)
+	if err != nil {
+		fmt.Println(err)
+		return
+	}
+
+	startMax := time.Now()
 
 	fmt.Println("Ищем максимальное значение в один поток")
-	// ваш код здесь
 
-	fmt.Printf("Максимальное значение элемента: %d\nВремя поиска: %d ms\n", max, elapsed)
+	max, err := maximum(data)
 
-	fmt.Printf("Ищем максимальное значение в %d потоков", CHUNKS)
-	// ваш код здесь
+	if err != nil {
+		fmt.Println("Ошибка поиска максимума в один поток:", err)
+	}
 
-	fmt.Printf("Максимальное значение элемента: %d\nВремя поиска: %d ms\n", max, elapsed)
+	elapsedMax := time.Since(startMax).Milliseconds()
+	fmt.Printf("Максимальное значение элемента: %d\nВремя поиска: %d ms\n", max, elapsedMax)
+
+	fmt.Printf("Ищем максимальное значение в %d потоков\n", CHUNKS)
+	startMaxChunks := time.Now()
+	maxChunks, err := maxChunks(data)
+	if err != nil {
+		fmt.Println("Ошибка поиска максимума в несколько потоков:", err)
+	}
+
+	elapsedMaxChunks := time.Since(startMaxChunks).Milliseconds()
+	fmt.Printf("Максимальное значение элемента: %d\nВремя поиска: %d ms\n", maxChunks, elapsedMaxChunks)
 }
--- main_test.go
+++ main_test.go
@@ -1,3 +1,77 @@
 package main
 
-// Пишите тесты в этом файле
+import (
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+	"github.com/stretchr/testify/require"
+)
+
+// generator tests
+func TestGenerateRandomElements(t *testing.T) {
+	tests := []struct {
+		size    int
+		wantErr bool
+	}{
+		{0, true},            //zero case
+		{-10, true},          //negative case
+		{10, false},          //small size
+		{100_000_000, false}, //large size
+	}
+
+	for _, test := range tests {
+		data, err := generateRandomElements(test.size)
+
+		if test.wantErr {
+			require.Error(t, err)
+			require.Nil(t, data)
+		} else {
+			require.NoError(t, err)
+			require.NotNil(t, data)
+			assert.Len(t, data, test.size)
+		}
+
+	}
+}
+
+// Maximum
+func TestMaximumEmptySlice(t *testing.T) {
+	_, err := maximum([]int{})
+	require.Error(t, err)
+}
+
+func TestMaximumSingleElement(t *testing.T) {
+	res, err := maximum([]int{1})
+	require.NoError(t, err)
+	assert.Equal(t, 1, res)
+}
+
+func TestMaximumOK(t *testing.T) {
+	res, err := maximum([]int{1, 2, 3, 10, 0})
+	require.NoError(t, err)
+	assert.Equal(t, 10, res)
+}
+
+// Maximum with many flows
+func TestMaxChunksEmpty(t *testing.T) {
+	_, err := maxChunks([]int{})
+	require.Error(t, err)
+}
+func TestMaxChunksSmallData(t *testing.T) {
+	data := []int{1, 2, 3, 10}
+	max, err := maxChunks(data)
+	require.NoError(t, err)
+	assert.Equal(t, 10, max)
+}
+
+func TestMaxChunksOK(t *testing.T) {
+	data := make([]int, 10000)
+	for i := range data {
+		data[i] = i
+	}
+	maxWant := 111_111
+	data[10] = maxWant
+	max, err := maxChunks(data)
+	require.NoError(t, err)
+	assert.Equal(t, maxWant, max)
+}
